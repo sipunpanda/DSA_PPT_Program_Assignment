2278ec7868f80162e38f8349b37526b1e5837968
Sipun <123020143+sipunpanda@users.noreply.github.com>
2023-06-23 23:36:36 +0530

    Update Q1.js

--- 01_DSA_ARRAY/Q1.js
+++ 01_DSA_ARRAY/Q1.js
@@ -1,3 +1,16 @@
+// 💡 **Q1.** Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+// You can return the answer in any order.
+
+// **Example:**
+// Input: nums = [2,7,11,15], target = 9
+// Output0 [0,1]
+
+// **Explanation:** Because nums[0] + nums[1] == 9, we return [0, 1][
+
+
 let num2 = [2, 7, 1, 15, 3, 4, 5, -2, 6, 8, 0, 9, 11]
 
 let target2 = 9;

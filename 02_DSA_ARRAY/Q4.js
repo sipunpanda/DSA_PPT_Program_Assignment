4d27e7b77f18b93f0a2e81addbf69b4f27f6b201
Sipun <123020143+sipunpanda@users.noreply.github.com>
2023-06-24 00:48:00 +0530

    Create Q4.js

---
+++ 02_DSA_ARRAY/Q4.js
@@ -0,0 +1,41 @@
+/* 
+Question 4
+You have a long flowerbed in which some of the plots are planted, and some are not.
+However, flowers cannot be planted in adjacent plots.
+Given an integer array flowerbed containing 0's and 1's, where 0 means empty and 1 means not empty,
+and an integer n, return true if n new flowers can be planted in the flowerbed without 
+violating the no-adjacent-flowers rule and false otherwise.
+
+Example 1:
+Input: flowerbed = [1,0,0,0,1], n = 1
+Output: true
+*/
+
+function canPlaceFlowers(flowerbed, n) {
+    let count = 0;
+    let i = 0;
+
+    while (i < flowerbed.length) {
+        if (
+            flowerbed[i] === 0 &&
+            (i === 0 || flowerbed[i - 1] === 0) &&
+            (i === flowerbed.length - 1 || flowerbed[i + 1] === 0)
+        ) {
+            flowerbed[i] = 1;
+            count++;
+        }
+
+        if (count >= n) {
+            return true;
+        }
+
+        i++;
+    }
+
+    return false;
+}
+
+let flowerbed = [1, 0, 0, 0, 1];
+let n = 1;
+
+console.log(canPlaceFlowers(flowerbed, n));  

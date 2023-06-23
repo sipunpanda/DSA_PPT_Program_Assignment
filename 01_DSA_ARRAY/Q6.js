77dc922926624702acaa187b6c59ecd9a3b084cb
Sipun <123020143+sipunpanda@users.noreply.github.com>
2023-06-24 00:27:46 +0530

    Create Q6.js

---
+++ 01_DSA_ARRAY/Q6.js
@@ -0,0 +1,24 @@
+// <aside>
+// 💡 **Q6.** Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+// **Example 1:**
+// Input: nums = [1,2,3,1]
+
+// Output: true
+
+// </aside>
+
+
+function distinct(nums) {
+    for (let i = 0; i < nums.length - 1; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[i] === nums[j]) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+let arr = [1, 2, 3, 1];
+console.log(distinct(arr)); 
